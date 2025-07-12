832787919a0f56912404050bbfad6b6ecec67bc9
Tejo Priya Boddu <boddutejopriya@gmail.com>
2025-07-12 23:30:35 +0530

    Create event_dataTypes.sv

---
+++ event_dataTypes.sv
@@ -0,0 +1,35 @@
+module events(); // Declare a new event called ack
+
+event ack;  // Declare done as alias to ack 
+
+event done = ack; // Event variable with no synchronization object
+
+event empty = null; 
+
+initial begin
+  $dumpfile("dump.vcd");
+  $dumpvars(0,events);
+  #1 -> ack;
+  #1 -> empty;
+  #1 -> done;
+  #1 $finish;
+end
+
+always @ (ack)
+begin
+  $display("ack event emitted");
+end
+
+always @ (done)
+begin
+  $display("done event emitted");
+end
+
+/*
+always @ (empty)
+begin
+  $display("empty event emitted");
+end
+*/
+
+endmodule
