8b0465629d3a493386bbf0ea99dab8f38e2f5ae7
Tejo Priya Boddu <boddutejopriya@gmail.com>
2025-08-06 14:30:45 +0530

    Create format_of_initial.sv

---
+++ Initial Block/format_of_initial.sv
@@ -0,0 +1,14 @@
+`timescale 1ns / 1ps
+module tb;
+  
+  reg a=0;
+  
+  initial a =1; //single line use of initial
+  
+  // multiline intial block
+  initial begin
+    a=1'b1;
+    #10;
+    a=1'b0;
+  end 
+endmodule
